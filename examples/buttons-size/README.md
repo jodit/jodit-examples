22025c08021fd4df1e209319aa0b5bc5ed245bc9
xdan <chupurnov@gmail.com>
2024-05-07 01:33:37 +0300

    Added Buttons size example

---
+++ examples/buttons-size/README.md
@@ -0,0 +1,10 @@
+# Change the size of the buttons
+
+You can change the size of the buttons by adding the `toolbarButtonSize`. The `toolbarButtonSize` attribute can be set to 'tiny' | 'xsmall' | 'small' | 'middle' | 'large'.
+
+```js
+const editor = new Jodit('#editor', {
+	buttons: 'bold,italic,underline',
+	toolbarButtonSize: 'small'
+});
+```
---
+++ examples/buttons-size/src/index.tsx
@@ -0,0 +1,45 @@
+import React, { MouseEvent } from 'react';
+import { createRoot } from 'react-dom/client';
+import JoditEditor, { Jodit } from 'jodit-react';
+
+import './index.css';
+
+type SizeType = 'tiny' | 'xsmall' | 'small' | 'middle' | 'large';
+const sizes: SizeType[] = ['tiny', 'xsmall', 'small', 'middle', 'large'];
+
+function App() {
+	const [size, setSize] = React.useState<SizeType>('middle');
+
+	const config = React.useMemo(
+		() => ({
+			toolbarButtonSize: size
+		}),
+		[size]
+	);
+
+	const onClick = React.useCallback((e: MouseEvent) => {
+		if (e.target instanceof HTMLElement) setSize(e.target.dataset.size as SizeType);
+	}, []);
+
+	return (
+		<div>
+			<div className={'button-group'}>
+				{sizes.map((s) => (
+					<button
+						onClick={onClick}
+						data-size={s}
+						key={s}
+						className={`button button-${s}`}
+						aria-pressed={s === size}
+					>
+						{s}
+					</button>
+				))}
+			</div>
+			<JoditEditor config={config} value={''} />
+		</div>
+	);
+}
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
